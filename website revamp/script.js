1a26b9d0fa111e1238d19dd99cf65be813284bce
nicohhle <nnguy099@ucr.edu>
2021-04-26 03:11:09 -0700

    website redo: new page design - home completed

---
+++ website revamp/script.js
@@ -0,0 +1,7 @@
+// typing animation
+let typed = new Typed(".typing", {
+  strings: ["software engineer", "design enthusiast", "hobby hunter"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
